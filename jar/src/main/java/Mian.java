8b5fe24ad7acd5ead2578bf67649e6cd25421f0d
leo <zxg88166@vip.qq.com>
2018-09-12 19:22:17 +0800

    Initial commit

---
+++ jar/src/main/java/Mian.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class Mian {
+
+
+    public static void main(String[] args) {
+        Frame f = new Frame("text");
+        f.setBounds(30,30,250,250);
+        f.setVisible(true);
+    }
+}
